333ac0f0b8f491b9c0fba88bb2c54ecfacd52815
Thillai N <judearasu@gmail.com>
2026-07-19 20:44:23 -0400

    feat(spiralGrid): add initial implementation of spiral grid function

---
+++ cassw/spiralGrid.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that takes a non-negative integer n and prints the numbers 0 through n in a clockwise spiral, 
+ * starting at the top-left of a square grid. Hint: The grid size should be ceil(sqrt(n + 1)), 
+ * and any unused cells should be blank.
+ * 
+ */
+
+function spiralGrid (){
+
+}
+
+console.log(30);
